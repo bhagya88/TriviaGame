a073de096769c2be2d93a478f22319c9489b25f9
bhagya <bhagya@outlook.com>
2016-08-30 17:26:15 -0500

    added logic for correct answers, wrong answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,51 @@
 $(document).ready(function(){
 	var timeRemaining;
 	var questionIntervalId;
-	var answerTimeoutId;
-	var questions=[{
-					question:"Question1",
-					answer:"Answer1",
-					options:["Answer2","Answer1","Answer3","Answer5"]},
+		
+	var correctAnswers;
+	var wrongAnswers;
+
+	var currentQuestionIndex;
+	var currentAnswer;
+
+	var questions = [{
+					question:"Where is Taj Mahal?",
+					answerIndex:1,
+					options:["Pakistan","India","Tibet","Indonesia"],
+					pic:"tajmahal.jpg"
+					},
+					{ 
+					question:"What is the capital of Argentina?",
+					answerIndex:0,
+					options:["Buenos Aires","Lima","Rio","Trinidad"],
+					pic:"rio.jpg"},
 					{
-					question:"Question2",
-					answer:"Answer2",
-					options:["Answer2","Answer1","Answer3","Answer5"]},
+					question:"Mt Rainier is located in this state.",
+					answerIndex:2,
+					options:["Texas","Oregon","Montana","Washigton"],
+					pic:"rainier.jpg"},
 					{
-					question:"Question3",
-					answer:"Answer1",
-					options:["Answer2","Answer1","Answer3","Answer5"]},
-					{
-					question:"Question4",
-					answer:"Answer1",
-					options:["Answer2","Answer1","Answer3","Answer4"]}
+					question:"2016 Olympics happened in this city.",
+					answerIndex:3,
+					options:["Beiging","Tokyo","Seoul","Rio"],
+					pic:"rio.jpg"}
 					];
-	var q= [];
+	var q = [];
+
+
 	function initializeQuiz(){
-		for(var i=0;i<questions.length;i++){
-		q.push(i);
+		for(var i = 0; i<questions.length; i++){
+			q.push(i);
+		}
+		$('#timer').show();
+	    correctAnswers = 0;
+	    wrongAnswers = 0;
+	    $('#msg1').html("");
+	    $('#msg2').html("");
+
 	}
-	$('#timer').show();
+
 	
-	}
 
 	function startQuiz(){
 		$('#start').hide();	
@@ -39,30 +58,48 @@ $(document).ready(function(){
 	    initializeQuiz();
 	    getQuestion();	
 	}
+
 	function decrementTimer(){
 		
 		timeRemaining--;
+
 		$('#timer').html("<p>Time remaining: "+timeRemaining+" sec."+"</p>");
+		console.log("inside decrement timer");
 		if(timeRemaining === 0 && questionIntervalId){
+			console.log("Timeup");
+			$('#msg1').html("Time is up!");
+			$('#msg2').html("The correct answer is "+currentAnswer);
+			console.log(questions[currentQuestionIndex].pic);
+			$('#picture').attr("src","assets/images/"+questions[currentQuestionIndex].pic);
 			getAnswer();
 			return;
 		}
 	}
 
 	function getQuestion(){
+
 			if(q.length>0){
-			var randomNum=Math.floor(Math.random()*q.length);
-			console.log("Random number",randomNum);
-			var currentQuestion=questions[q[randomNum]].question;
-			//$("input[name=interview]:checked").val()
-			q.splice(randomNum,1);
-			console.log("arr",q);
-			$('#ques').html(currentQuestion);
-			$('#question').show();
-			$('#answer').hide();
-			
-			timeRemaining = 10;
-			questionIntervalId = setInterval(decrementTimer,1000);
+
+				var randomNum = Math.floor(Math.random()*q.length);
+				currentQuestionIndex = q[randomNum];
+
+				console.log("Random number",randomNum);
+				var currentQuestion=questions[currentQuestionIndex].question;
+				currentAnswer = questions[currentQuestionIndex].options[questions[currentQuestionIndex].answerIndex];
+				
+				$('.option').eq(0).html(questions[currentQuestionIndex].options[0]);
+				$('.option').eq(1).html(questions[currentQuestionIndex].options[1]);
+				$('.option').eq(2).html(questions[currentQuestionIndex].options[2]);
+				$('.option').eq(3).html(questions[currentQuestionIndex].options[3]);
+				
+				q.splice(randomNum,1);
+				console.log("arr",q);
+				$('#ques').html(currentQuestion);
+				$('#answer').hide();
+				$('#question').show();
+				
+				timeRemaining =3;
+				questionIntervalId = setInterval(decrementTimer,1000);
 			}else{
 				getResult();
 			}
@@ -70,21 +107,50 @@ $(document).ready(function(){
 		
 
 	function getAnswer(){
+
+	
 		$('#question').hide();
+		console.log("before show");
 		$('#answer').show();
+		$('#main').append($('#answer'));
+		console.log("after show");
 		clearInterval(questionIntervalId);
-		answerTimeoutId = setTimeout(getQuestion,5000);
-		return;
+		setTimeout(getQuestion,2000);
+		
 	}
+
+	function checkAndGetAnswer(){
+
+		selectedOption=$(this).html();
+		
+
+		if(selectedOption === currentAnswer){
+			console.log("you chose correct answer",selectedOption);
+			$('#msg1').html("You got it!");
+			$('#msg2').html("");
+			correctAnswers++;
+		}else{
+			$('#msg1').html("Oops!");
+			$('#msg2').html("The correct answer is "+currentAnswer);
+			console.log("you chose wrong answer",selectedOption);
+			wrongAnswers++;
+		}
+		console.log(questions[currentQuestionIndex].pic);
+		$('#picture').attr("src","assets/images/"+questions[currentQuestionIndex].pic);
+		getAnswer();
+	}
+
 	function getResult(){
+
+		var unanswered = questions.length-correctAnswers-wrongAnswers;
 		console.log("inside result");
 		$('#answer').hide();
 		$('#result').show();
-		clearInterval(questionIntervalId);
-		return;
+		$('#main').append($('#result'));
+		$('#result>p').html('<p>correct answers: '+correctAnswers+'</p><p>wrong answers: '+wrongAnswers+'</p><p>Unanswered: '+unanswered+'</p');
 	}
 
-	
+
 
 	console.log(questions);
 
@@ -94,7 +160,7 @@ $(document).ready(function(){
 	$('#timer').hide();
 	
 	$('#btnStart').click(startQuiz);
-	$('#userAnswer').click(getAnswer);
+	$('#options').on('click','.option',checkAndGetAnswer);
 	$('#btnRestart').click(restartQuiz);
 
 
